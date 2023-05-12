1666e0dce677e4493bf600292d407f0a2ffea8d5
cwoods92 <charleswoods92a@gmail.com>
2023-05-12 13:29:59 -0700

    initial commit

---
+++ git_random_commits.sh
@@ -0,0 +1,33 @@
+# a million commits
+for Y in {2015..2015}
+do
+  mkdir $Y
+  cd $Y
+  for M in {01..5}
+  do
+    mkdir $M
+    cd $M
+    for D in {01..28}
+    do
+      mkdir $D
+      cd $D
+      count=$(( $RANDOM % 3 ));
+      for i in $(seq 1 $count)
+      do
+        echo "$i on $M/$D/$Y" > commit.md
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
+      done
+      cd ../
+    done
+    cd ../
+  done
+  cd ../
+done
+git push origin master
+git rm -rf 20**
+git rm -rf 19**
+git commit -am "cleanup"
+git push origin master
